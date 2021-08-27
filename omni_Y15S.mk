e8f7344c40b17b6cc413d9fc7afa4d4e32a47474
Hemanth <hemanthjabalpuri@gmail.com>
2021-08-27 12:02:05 +0530

    Y15S: Misc fix

--- omni_Y15S.mk
+++ omni_Y15S.mk
@@ -1,5 +1,5 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_DEVICE := Y15S
